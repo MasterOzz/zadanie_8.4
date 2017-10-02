6ab0ebc4fbe2941892b77bcb630380e9861c6c2d
Piotr Szczepanik <piotrszczepanik29@wp.pl>
2017-10-02 20:24:43 +0200

    pliki

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+// test
+var variable = 'test';
+
+console.log(variable);
+
+// witaj œwiecie
+ 
+var name = prompt('Enter your name !!!');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/*
+	Komentarz do zadania :)
+*/
